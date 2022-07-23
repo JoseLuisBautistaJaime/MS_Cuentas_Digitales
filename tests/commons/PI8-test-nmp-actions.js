d1ea71f923f60a0e9ca9e8a7efb5869e656de8f8
CristhianOdinRuedaJimenez <curedaji@montepiedad.com.mx>
2022-07-23 13:28:30 -0500

    MS_CUENTAS_DIGITALES, Descripcion: Mejoras

--- tests/commons/PI8-test-nmp-actions.js
+++ tests/commons/PI8-test-nmp-actions.js
@@ -1,39 +1,20 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-await-in-loop */
-import { ActivacionEventoService } from '../../src/services/ActivacionEvento.Service'
-import { clienteActivacionService } from '../../src/services/clienteActivacion.Service'
 import { ClienteService } from '../../src/services/Cliente.Service'
 import { ACTIVACION_BLOQUEO_REINTENTOS } from '../../src/commons/constants'
 import { LOG } from '../../src/commons'
+import { TEST_CLIENTE, TEST_CLIENTE_DATA } from './pi8-test-nmp-constants'
+import { ActivacionEventoService } from '../../src/services/ActivacionEvento.Service'
+import { clienteActivacionService } from '../../src/services/clienteActivacion.Service'
 
-export const HEADER = {
-  AUTHORIZATION: 'Basic zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz=',
-  IDCONSUMIDOR: 99,
-  IDDESTINO: 99,
-  USUARIO: 'test',
-  AUTHBEARER: 'xxxxxxxxxxxx='
-}
-
-export const TEST_CLIENTE = '9999'
-export const TEST_CLIENTE_DATA = {
-  idCliente: TEST_CLIENTE,
-  idDevice: '74312734d5403d54',
-  tarjetaMonte: '11111',
-  nombreCliente: 'ricoff',
-  apellidoPaterno: 'CARRILLO',
-  apellidoMaterno: 'LOPEZF',
-  correoCliente: 'rigocl@hotmail.com',
-  celularCliente: '6731143889'
-}
-
-const reiniciarCliente = async tag => {
+export const reiniciarCliente = async tag => {
   LOG.debug(tag)
   await ClienteService.removerCliente(TEST_CLIENTE)
   await ActivacionEventoService.removerEventos(TEST_CLIENTE)
   await ClienteService.actualizarCliente(TEST_CLIENTE_DATA)
 }
 
-const bloquearClienteConEnviosOtp = async tag => {
+export const bloquearClienteConEnviosOtp = async tag => {
   LOG.debug(tag)
   await ActivacionEventoService.removerEventos(TEST_CLIENTE)
   for (let i = 0; i < ACTIVACION_BLOQUEO_REINTENTOS; i++) {
@@ -41,7 +22,7 @@ const bloquearClienteConEnviosOtp = async tag => {
   }
 }
 
-const bloquearClienteSinEventos = async tag => {
+export const bloquearClienteSinEventos = async tag => {
   LOG.debug(tag)
   await clienteActivacionService.establecerEstatusActivacion(TEST_CLIENTE, 5)
   await ActivacionEventoService.removerEventos(TEST_CLIENTE)
@@ -50,7 +31,5 @@ const bloquearClienteSinEventos = async tag => {
 export const testHelpers = {
   reiniciarCliente,
   bloquearClienteConEnviosOtp,
-  bloquearClienteSinEventos,
-  TEST_CLIENTE,
-  TEST_CLIENTE_DATA
+  bloquearClienteSinEventos
 }
---
+++ tests/commons/pi8-test-nmp-constants.js
@@ -0,0 +1,18 @@
+export const TEST_CLIENTE = '9999'
+export const TEST_CLIENTE_DATA = {
+  idCliente: TEST_CLIENTE,
+  idDevice: '74312734d5403d54',
+  tarjetaMonte: '11111',
+  nombreCliente: 'ricoff',
+  apellidoPaterno: 'CARRILLO',
+  apellidoMaterno: 'LOPEZF',
+  correoCliente: 'rigocl@hotmail.com',
+  celularCliente: '6731143889'
+}
+export const HEADER = {
+  AUTHORIZATION: 'Basic zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz=',
+  IDCONSUMIDOR: 99,
+  IDDESTINO: 99,
+  USUARIO: 'test',
+  AUTHBEARER: 'xxxxxxxxxxxx='
+}
